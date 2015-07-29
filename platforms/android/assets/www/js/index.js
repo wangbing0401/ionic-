36c4353834ded1eaa07bbb16fbbbdcb0ad5fdb67
wangb <810376348@qq.com>
2015-07-29 11:00:12 +0800

    ng-cordova

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,4 +1,4 @@
-var app = angular.module('myapp', ['ionic']);
+var app = angular.module('myapp', ['ionic', 'ngCordova']);
 
 app.config(['$stateProvider', '$urlRouterProvider', '$ionicConfigProvider', function($stateProvider, $urlRouterProvider, $ionicConfigProvider){
 
@@ -25,6 +25,15 @@ app.config(['$stateProvider', '$urlRouterProvider', '$ionicConfigProvider', func
                 'home-tab': {
                     templateUrl: "templates/push.html",
                     controller: "PushController"
+                }
+            }
+        })
+        .state('tabs.scroll', {
+            url:"/scroll",
+            views:{
+                'home-tab':{
+                    templateUrl:"templates/scroll.html",
+                    controller:"ScrollController"
                 }
             }
         })
@@ -56,12 +65,82 @@ app.controller('HomeTabController', ['$scope', '$rootScope', '$state', function(
         $rootScope.hideTabs = true;
         $state.go('tabs.push');
     }
+    $scope.push_scroll = function(){
+        $rootScope.hideTabs = true;
+        $state.go('tabs.scroll');
+    }
 }]);
-app.controller('PushController', ['$scope', '$rootScope', function($scope, $rootScope){
+app.controller('PushController', ['$scope', '$rootScope', '$cordovaActionSheet', '$cordovaCamera', '$cordovaBarcodeScanner', function($scope, $rootScope, $cordovaActionSheet, $cordovaCamera, $cordovaBarcodeScanner){
+    var sheet_options = {
+        buttonLabels: ['相机', '相册'],
+        addCancelButtonWithLabel: '取消',
+        androidEnableCancelButton : true,
+        winphoneEnableCancelButton : true,
+    };
+
+    var camera_options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 100,
+        targetHeight: 100,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false
+    };
+
+    var photo_options = {
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
+    };
+
+    document.addEventListener("deviceready", function () {
+        $scope.sheetShow = function(){
+            $cordovaActionSheet.show(sheet_options).then(function(btnIndex){
+                if(btnIndex == 1){
+                    $cordovaCamera.getPicture(camera_options).then(function(imageData){
+                        alert(imageData);
+                    });
+                }
+                if(btnIndex == 2){
+                    $cordovaCamera.getPicture(photo_options).then(function(imageUrI){
+                        alert(imageUrI);
+                    });
+                }
+            });
+        }
+        $scope.tiaoxingma = function(){
+            $cordovaBarcodeScanner.scan().then(function(barcodeData){
+                alert(barcodeData.lastName);
+            }, function(error){
+
+            });
+        }
+    }, false);
     $scope.fanhui = function(){
         $rootScope.hideTabs = false;
         window.history.back();
     }
+}]);
+app.controller('ScrollController', ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout){
+    $scope.fanhui = function(){
+        $rootScope.hideTabs = false;
+        window.history.back();
+    }
+    $scope.data = [1,2,3,4,5];
+    var length;
+    $scope.doRefresh = function() {
+        length = $scope.data.length;
+        length++;
+        $scope.data.push(length);
+        $timeout( function() {
+            //Stop the ion-refresher from spinning
+            $scope.$broadcast('scroll.refreshComplete');
+
+        }, 1000);
+
+    };
 }]);
 app.controller('DetailTabController', ['$scope', function($scope){
     var socket = io('http://192.168.1.113:3000');
@@ -85,7 +164,30 @@ app.controller('DetailTabController', ['$scope', function($scope){
     });
 }]);
 
-app.controller('SettingTabController', ['$scope', function($scope){
+app.controller('SettingTabController', ['$scope', '$cordovaCapture', '$cordovaTouchID', function($scope, $cordovaCapture, $cordovaTouchID){
+    document.addEventListener("deviceready", function () {
+        $cordovaTouchID.checkSupport().then(function(){
+            $cordovaTouchID.authenticate("确认支付吗").then(function() {
+                alert("成功");
+            }, function () {
+                alert("失败");
+            });
+        }, function(error){
+            alert("失败啦");
+        });
+
+        $scope.captureVideo = function() {
+            var options = { limit: 3, duration: 15 };
+
+            $cordovaCapture.captureVideo(options).then(function(videoData) {
+                $("#wb_video").attr('src', videoData);
+                $("#wb_video").attr('autoplay', 'true');
+            }, function(err) {
+                // An error occurred. Show a message to the user
+            });
+        }
+    }, false);
+
 }]);
 
 app.controller('sideMenuController', ['$scope', '$rootScope', function($scope, $rootScope){
